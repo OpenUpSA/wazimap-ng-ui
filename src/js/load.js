faa36b0ba66dfcfca93023bface34a6fef4bf860
Adi Eyal <adi@openup.org.za>
2020-01-01 00:56:47 +0200

    Refactored menu - moved into its own file

--- src/js/load.js
+++ src/js/load.js
@@ -2,6 +2,7 @@ import {select as d3select} from 'd3-selection';
 import {format as d3format} from 'd3-format';
 import Controller from './controller';
 import loadProfile from './page_profile';
+import {loadMenu} from './menu';
 import {MapControl} from './maps';
 import {getJSON} from './utils';
 
@@ -10,68 +11,6 @@ import "../css/barchart.css";
 
 //const baseUrl = "https://wazimap-ng.openup.org.za";
 const baseUrl = "http://localhost:8000";
-
-function loadMenu(data) {
-    var parentContainer = $(".data-menu__links")
-
-    function setActive(el) {
-        resetActive();
-        $(this).addClass("menu__link_h4--active")
-    }
-
-    function resetActive() {
-        $(".menu__link_h4--active", parentContainer).removeClass("menu__link_h4--active");
-    }
-
-    var categoryTemplate = $(".data-menu__category")[0].cloneNode(true);
-    $(".data-menu__category").remove();
-
-    for (const [category, subcategories] of Object.entries(data)) {
-        var newCategory = categoryTemplate.cloneNode(true)
-        $(".link__h1_title div", newCategory).text(category);
-        parentContainer.append(newCategory);
-        var h2Wrapper = $(".category__dropdown_wrapper", newCategory);
-        var subCategoryTemplate = $(".data-menu__subcategory", h2Wrapper)[0].cloneNode(true);
-        $(".data-menu__subcategory", h2Wrapper).remove();
-
-        for (const [subcategory, indicators] of Object.entries(subcategories)) {
-            var newSubCategory = subCategoryTemplate.cloneNode(true);
-            $(".data-menu__sub-category_trigger div", newSubCategory).text(subcategory);
-            h2Wrapper.append(newSubCategory);
-
-            var h3Wrapper = $(".sub-category__dropdown_wrapper", newSubCategory);
-            var indicatorTemplate = $(".data-menu__indicator", h3Wrapper)[0].cloneNode(true);
-
-            $(".data-menu__indicator", h3Wrapper).remove();
-            for (const [indicator, subindicators] of Object.entries(indicators)) {
-                var indicatorLabel = indicator;
-                var newIndicator = indicatorTemplate.cloneNode(true);
-                $(".data-menu__indicator_trigger div", newIndicator).text(indicatorLabel);
-                h3Wrapper.append(newIndicator);
-
-                var indicatorWrapper = $(".indicator__dropdown_wrapper", newIndicator);
-                var subIndicatorTemplate = $(".data-menu__sub-indicator", newIndicator)[0].cloneNode(true);
-
-                $(".data-menu__sub-indicator", indicatorWrapper).remove();
-                $(".menu__link_h4--active", indicatorWrapper).remove();
-                //if (subindicators.classes != undefined) {
-                if (true) {
-
-                    subindicators.forEach(function(obj) {
-                        var newSubIndicator = subIndicatorTemplate.cloneNode(true);
-                        var text = obj.key
-                        $("div:nth-child(2)", newSubIndicator).text(text);
-                        indicatorWrapper.append(newSubIndicator);
-
-                        $(newSubIndicator).on("click", setActive);
-                    })
-                } else {
-                    indicatorWrapper.remove();
-                }
-            }
-        };
-    }
-}
 
 function loadGeography(profileId, geographyId) {
     var url = baseUrl + "/api/v1/profiles/" + profileId + "/geographies/" + geographyId + "/"
@@ -94,7 +33,7 @@ export default function load(profileId) {
     mapcontrol.on("geoselect", function(areaCode) {
         controller.setGeography(areaCode);
     })
-    
+
     controller.trigger()
     // TODO need to set this to the geography searched for
     mapcontrol.overlayBoundaries(null);
---
+++ src/js/menu.js
@@ -0,0 +1,73 @@
+export function loadMenu(data) {
+    var parentContainer = $(".data-menu__links")
+    var categoryTemplate = $(".data-menu__category")[0].cloneNode(true);
+    var subCategoryTemplate = $(".data-menu__subcategory", categoryTemplate)[0].cloneNode(true);
+    var indicatorTemplate = $(".data-menu__indicator", subCategoryTemplate)[0].cloneNode(true);
+
+    function addSubIndicators(wrapper, indicator, subindicators) {
+        var indicatorLabel = indicator;
+        var newIndicator = indicatorTemplate.cloneNode(true);
+        $(".data-menu__indicator_trigger div", newIndicator).text(indicatorLabel);
+        wrapper.append(newIndicator);
+
+        var indicatorWrapper = $(".indicator__dropdown_wrapper", newIndicator);
+        var subIndicatorTemplate = $(".data-menu__sub-indicator", newIndicator)[0].cloneNode(true);
+
+        $(".data-menu__sub-indicator", indicatorWrapper).remove();
+        $(".menu__link_h4--active", indicatorWrapper).remove();
+        // TODO ????
+        if (true) {
+            subindicators.forEach(function(obj) {
+                var newSubIndicator = subIndicatorTemplate.cloneNode(true);
+                var text = obj.key
+                $("div:nth-child(2)", newSubIndicator).text(text);
+                indicatorWrapper.append(newSubIndicator);
+
+                $(newSubIndicator).on("click", setActive);
+            })
+        } else {
+            indicatorWrapper.remove();
+        }
+    }
+
+    function addIndicators(wrapper, subcategory, indicators) {
+        var newSubCategory = subCategoryTemplate.cloneNode(true);
+        $(".data-menu__sub-category_trigger div", newSubCategory).text(subcategory);
+        wrapper.append(newSubCategory);
+
+        var h3Wrapper = $(".sub-category__dropdown_wrapper", newSubCategory);
+
+        $(".data-menu__indicator", h3Wrapper).remove();
+        for (const [indicator, subindicators] of Object.entries(indicators)) {
+            addSubIndicators(h3Wrapper, indicator, subindicators);
+        }
+    }
+
+    function addSubcategories(wrapper, category, subcategories) {
+        var newCategory = categoryTemplate.cloneNode(true)
+        $(".link__h1_title div", newCategory).text(category);
+        parentContainer.append(newCategory);
+        var h2Wrapper = $(".category__dropdown_wrapper", newCategory);
+        $(".data-menu__subcategory", h2Wrapper).remove();
+
+        for (const [subcategory, indicators] of Object.entries(subcategories)) {
+            addIndicators(h2Wrapper, subcategory, indicators);
+        };
+
+    }
+
+    function setActive(el) {
+        resetActive();
+        $(this).addClass("menu__link_h4--active")
+    }
+
+    function resetActive() {
+        $(".menu__link_h4--active", parentContainer).removeClass("menu__link_h4--active");
+    }
+
+    $(".data-menu__category").remove();
+
+    for (const [category, subcategories] of Object.entries(data)) {
+        addSubcategories(parentContainer, category, subcategories)
+    }
+}

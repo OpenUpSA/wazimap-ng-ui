4419ce21f4265ee7ce69d0963e914d2b1057e92c
Adi Eyal <adi@openup.org.za>
2020-01-18 06:48:27 +0200

    Updated index.html and profile code

--- src/js/page_profile.js
+++ src/js/page_profile.js
@@ -2,39 +2,61 @@ import {select as d3select} from 'd3-selection';
 import {format as d3format} from 'd3-format';
 import {reusableBarChart} from "data-visualisations/src/charts/bar/reusable-bar-chart/reusable-bar-chart";
 
-const profileWrapper = $(".location-profile");
-const categoryTemplate = $(".location-profile__data-category--first")[0].cloneNode(true);
-const subcategoryTemplate = $(".data-category__indicator", categoryTemplate)[0].cloneNode(true);
-const indicatorTemplate = $(".indicator__chart", subcategoryTemplate)[0].cloneNode(true);
-const breadcrumbsContainer = $(".location-header__breadcumbs_wrapper", profileWrapper);
+const profileHeaderClass = '#profile-top';
+const categoryClass = '.data-category';
+const categoryHeaderClass = '.data-category__header';
+const categoryHeaderTitleClass = `${categoryHeaderClass} h2`;
+const categoryHeaderSubtitleClass = '.data-category__header_subtitle div';
+const categoryHeaderDescriptionClass = '.data-category__header_description p';
+
+const subcategoryClass = '.data-category__indicator';
+const subcategoryTitleClass = '.indicator__title_wrapper h3';
+const subcategoryDescriptionClass = '.indicator__description p.paragraph';
+const subcategoryMetricsClass = '.indicator__key-metrics';
+
+const indicatorClass = '.indicator__sub-indicator';
+const indicatorTitleClass = '.sub-indicator__chart_header h4';
+
+const chartContainerClass = '.sub-indicator__chart';
+
+const headerTitleClass = '.location-header__title h1';
+const breadcrumbsContainerClass = '.location-header__breadcrumbs';
+const breadcrumbClass = '.breadcrumb';
+
+const profileHeader = $(profileHeaderClass);
+const categoryTemplate = $(categoryClass)[0].cloneNode(true);
+const subcategoryTemplate = $(subcategoryClass, categoryTemplate)[0].cloneNode(true);
+
+const indicatorTemplate = $(indicatorClass, subcategoryTemplate)[0].cloneNode(true);
+const breadcrumbsContainer = $(breadcrumbsContainerClass, profileHeader);
 const breadcrumbTemplate = $(".breadcrumb", breadcrumbsContainer)[0].cloneNode(true);
-const metricWrapper = $(".key-metrics", profileWrapper)
-const metricTemplate = $(".key-metric", metricWrapper)[0].cloneNode(true)
+const metricWrapper = $(".location-header__key-metrics", profileHeader);
+const metricTemplate = $(".key-metric", metricWrapper)[0].cloneNode(true);
 
 function updateGeography(container, data) {
-    var geography = data.geography
-    var label = geography.name + " (" + geography.code + ")";
-    $(".location-header_title h1", container).text(label);
+    const geography = data.geography
+    const label = geography.name + " (" + geography.code + ")";
+    $(headerTitleClass, container).text(label);
 
     addBreadCrumbs(breadcrumbsContainer, geography);
 }
 
 function addBreadCrumbs(container, geography) {
-    $(".breadcrumb", container).remove();
+    $(breadcrumbClass, container).remove();
 
     geography.parents.forEach((el) => {
-        var breadcrumb = breadcrumbTemplate.cloneNode(true);
+        let breadcrumb = breadcrumbTemplate.cloneNode(true);
         $(".truncate", breadcrumb).text(el.name) 
         container.append(breadcrumb);
     })
 }
 
 function addKeyMetrics(container, data) {
-    var metrics = data.key_metrics
+    const metrics = data.key_metrics
     $(".key-metric", metricWrapper).remove()
 
     metrics.forEach((el) => {
-        var metric = metricTemplate.cloneNode(true)
+        let metric = metricTemplate.cloneNode(true)
         $(".key-metric_value div", metric).text(el.value)
         $(".key-metric_title", metric).text(el.label)
         metricWrapper.append(metric)
@@ -43,33 +65,27 @@ function addKeyMetrics(container, data) {
 
 
 export default function loadProfile(data) {
-    var all_indicators = data.indicators;
-
-    $(".location-profile__data-category--first").remove();
-    $(".location-profile__data-category").remove();
-    $(".data-category__indicator", categoryTemplate).remove();
-    $(".data-category__indicator--last", categoryTemplate).remove();
-    $(".indicator__chart", subcategoryTemplate).remove();
-
-    updateGeography(profileWrapper, data);
-    addKeyMetrics(profileWrapper, data);
-
 
     function addCategory(category, subcategories) {
-        var newCategorySection = categoryTemplate.cloneNode(true);
-        var wrapper = $(".grid__module_section", newCategorySection);
+        const newCategorySection = categoryTemplate.cloneNode(true);
+        const wrapper = newCategorySection;
 
-        $(".data-category__header_title h2", newCategorySection).text(category);
-        profileWrapper.append(newCategorySection);
+        $(categoryHeaderTitleClass, newCategorySection).text(category);
+        // $(categoryHeaderSubtitleClass, newCategorySection).text(category.subTitle);
+        // $(categoryHeaderDescriptionClass, newCategorySection).text(category.description);
+
+        profileHeader.append(newCategorySection);
         for (const [subcategory, indicators] of Object.entries(subcategories)) {
             addSubcategory(wrapper, subcategory, indicators);
         }
     }
 
     function addSubcategory(wrapper, subcategory, indicators) {
-        var newSubcategorySection = subcategoryTemplate.cloneNode(true);
-        $(".indicator__title_wrapper h3", newSubcategorySection).text(subcategory);
+        const newSubcategorySection = subcategoryTemplate.cloneNode(true);
+        $(subcategoryTitleClass, newSubcategorySection).text(subcategory);
+        //$(subcategoryDescriptionClass).text(subcategory.description);
         wrapper.append(newSubcategorySection);
+        //$(subcategoryMetricsClass).dosomethihn
 
         for (const [indicator, classes] of Object.entries(indicators)) {
             addIndicator(newSubcategorySection, indicator, classes);
@@ -77,10 +93,11 @@ export default function loadProfile(data) {
     }
 
     function addIndicator(wrapper, indicator, classes) {
-        var newIndicatorSection = indicatorTemplate.cloneNode(true);
-        $(".indicator__chart_title h4", newIndicatorSection).text(indicator);
+        const newIndicatorSection = indicatorTemplate.cloneNode(true);
+        const chartContainer = $(chartContainerClass, newIndicatorSection);
+
+        $(indicatorTitleClass, newIndicatorSection).text(indicator);
         wrapper.append(newIndicatorSection);
-        var chartContainer = $(".indicator__chart_container", newIndicatorSection);
 
         classes.forEach((el) => {
             el["label"] = el.key
@@ -90,8 +107,12 @@ export default function loadProfile(data) {
     }
 
     function addChart(container, data) {
-        var fmt = d3format(",.2f");
+        // TODO need to hander different chart types
+        const chartType = 'bar';
+        const chartClass = `${chartContainerClass}--${chartType}`;
+        const fmt = d3format(",.2f");
         const myChart = reusableBarChart();
+
         // TODO how big should this be?
         myChart.height(100);
         myChart.width(800);
@@ -107,6 +128,16 @@ export default function loadProfile(data) {
             .call(myChart.data(data));
 
     }
+
+    const all_indicators = data.indicators;
+
+    $(categoryClass).remove();
+    $(subcategoryClass, categoryTemplate).remove();
+    $(indicatorClass, subcategoryTemplate).remove();
+
+    updateGeography(profileHeader, data);
+    addKeyMetrics(profileHeader, data);
+
 
     for (const [category, subcategories] of Object.entries(all_indicators)) {
         addCategory(category, subcategories);

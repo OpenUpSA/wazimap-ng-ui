7f2c436ce451f21169950f6d8a694ccec819ea8c
Pieter Fourie <pfourie@edisc.digital>
2020-01-22 14:41:56 +0200

    Improvements on loading_spinner

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import load from "./load";
 
 var profileId = 1;
 var baseUrl = "https://wazimap-ng.openup.org.za";
-//if (window.location.href.search("localhost") > 0)
-//    baseUrl = "http://localhost:8000";
+if (window.location.href.search("localhost") > 0)
+    baseUrl = "http://localhost:8000";
 
 load(baseUrl, profileId);

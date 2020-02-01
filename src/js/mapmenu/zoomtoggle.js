3dc00c60341ac097a1738720cfe059985703dd90
Adi Eyal <adi@openup.org.za>
2020-02-01 10:33:04 +0200

    Integrated map zoom with toggle

---
+++ src/js/mapmenu/zoomtoggle.js
@@ -0,0 +1,32 @@
+import {Observable} from '../utils';
+
+const zoomToggleClass = '.map-settings__list-item_checkbox .switch';
+
+export class ZoomToggle extends Observable {
+    constructor() {
+        super();
+        this.enabled = false;
+        this.state = 0;
+        console.log("ZoomToggle constructor")
+
+        this.prepareDOMElements();
+    }
+
+
+    prepareDOMElements() {
+        const self = this;
+        this.zoomToggle = $(zoomToggleClass); 
+        this.zoomToggle.on("click", e => {
+            // TODO need to figure out why there are two clicks
+            // currently ignoring the second click
+            if (this.state == 0){
+                this.state = 1
+                self.enabled = !self.enabled;
+                console.log(`Zoom is ${self.enabled}`)
+                self.triggerEvent("zoomToggled", self);
+            } else if (this.state == 1) {
+                this.state = 0;
+            }
+        });
+    }
+}

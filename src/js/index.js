24c4809688aff2838d00fff753310dee16e845b3
Adi Eyal <adi@openup.org.za>
2020-02-03 09:19:35 +0200

    Added staging url to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,9 @@ import configureApplication from "./load";
 
 var profileId = 1;
 var baseUrl = "https://wazimap-ng.openup.org.za";
-if (window.location.href.search("localhost") > 0)
+if (window.location.href.search("localhost") >= 0)
     baseUrl = "http://localhost:8000";
+else if (windows.location.href.search("staging") >= 0)
+    baseUrl = "https://staging.wazimap-ng.openup.org.za"
 
 configureApplication(baseUrl, profileId);

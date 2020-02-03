18348dbe321cb0631d3d87b857e2d929732b33b4
Adi Eyal <adi@openup.org.za>
2020-02-03 09:25:17 +0200

    Added staging site

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ var profileId = 1;
 var baseUrl = "https://wazimap-ng.openup.org.za";
 if (window.location.href.search("localhost") >= 0)
     baseUrl = "http://localhost:8000";
-else if (windows.location.href.search("staging") >= 0)
+else if (window.location.href.search("staging") >= 0)
     baseUrl = "https://staging.wazimap-ng.openup.org.za"
 
 configureApplication(baseUrl, profileId);

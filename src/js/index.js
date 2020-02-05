90d2d97947cd3ede69964fc0d95270db7aba1902
emreyldrm <emre.yildirim@inpds.com>
2020-02-06 02:35:42 +0300

    re-drawing the borders when geo view is changed at muni level

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import configureApplication from "./load";
 var profileId = 1;
 var baseUrl = "https://wazimap-ng.openup.org.za";
 if (window.location.href.search("localhost") >= 0)
-    baseUrl = "http://localhost:8000";
+    baseUrl = "https://staging.wazimap-ng.openup.org.za"
 else if (window.location.href.search("staging") >= 0)
     baseUrl = "https://staging.wazimap-ng.openup.org.za"
 

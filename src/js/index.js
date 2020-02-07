57ff7163f3193c256815d6cc24e2f7d8793be8aa
Adi Eyal <adi@openup.org.za>
2020-02-07 11:39:22 +0200

    Updated baseUrl router

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,13 @@
 import configureApplication from "./load";
 
-var profileId = 1;
-var baseUrl = "https://wazimap-ng.openup.org.za";
-if (window.location.href.search("localhost") >= 0)
+let profileId = 1;
+let baseUrl;
+
+if (window.location.href.search("staging") >= 0)
     baseUrl = "https://staging.wazimap-ng.openup.org.za"
-else if (window.location.href.search("staging") >= 0)
+else if (window.location.href.search("localhost") >= 0)
     baseUrl = "https://staging.wazimap-ng.openup.org.za"
+else
+    baseUrl = "https://wazimap-ng.openup.org.za";
 
 configureApplication(baseUrl, profileId);

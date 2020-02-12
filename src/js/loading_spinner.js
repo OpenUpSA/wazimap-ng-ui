6958e636e1e23c347f8463c014d2ade47208553d
emreyldrm <emre.yildirim@inpds.com>
2020-02-12 17:50:05 +0300

    added a spinner for map loading

--- src/js/loading_spinner.js
+++ src/js/loading_spinner.js
@@ -1,24 +1,38 @@
 //import {Observable} from './utils';
 
+let removeClsName = 'hide';
+
 /**
  * Keeps track of hiding or showing a spinner
  */
 export class LoadingSpinner {
-    constructor(spinnerDOM, SpinnerState = { start: false, stop: false}) {
+	constructor(spinnerDOM, SpinnerState = {start: false, stop: false}, useHideClass = false) {
 		this.spinnerObject = $(spinnerDOM);
-		
-		if(SpinnerState.start)
+		this.useHideClass = useHideClass;
+		this.spinnerDOM = spinnerDOM;
+
+		if (SpinnerState.start)
 			this.start();
-		else if(SpinnerState.stop)
+		else if (SpinnerState.stop)
 			this.stop();
-    }
-	
-	start(){
-		this.spinnerObject.show();
 	}
 
-	stop(){
-		this.spinnerObject.hide();
+	start() {
+		if (this.useHideClass) {
+			$(this.spinnerDOM).removeClass(removeClsName);
+		} else {
+			this.spinnerObject.show();
+		}
+	}
+
+	stop() {
+		if (this.useHideClass) {
+			if (!this.spinnerObject.hasClass(removeClsName)) {
+				$(this.spinnerDOM).addClass(removeClsName);
+			}
+		} else {
+			this.spinnerObject.hide();
+		}
 	}
 
 }

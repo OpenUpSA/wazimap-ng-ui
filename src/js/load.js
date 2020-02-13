0c60d08ab4111aadf66907fcbe9ef143a52bf552
emreyldrm <emre.yildirim@inpds.com>
2020-02-13 21:08:26 +0300

    showing the legend according to the selected subindicator

--- src/js/load.js
+++ src/js/load.js
@@ -46,7 +46,7 @@ export default function configureApplication(serverUrl, profileId) {
     // TODO not certain if it is need to register both here and in the controller in loadedGeography
     controller.registerWebflowEvents();
     controller.on('subindicatorClick', payload => mapcontrol.choropleth(payload.state.subindicator))
-    controller.on('subindicatorClick', payload => mapchip.onSubIndicatorChange(payload.payload));
+    controller.on('subindicatorClick', payload => mapchip.onSubIndicatorChange(payload.payload, mapcontrol.layerCache));
     controller.on('layerMouseOver', payload => popup.loadPopup(payload));
     controller.on('layerMouseOut', payload => popup.hidePopup(payload));
     controller.on('layerMouseMove', payload => popup.updatePopupPosition(payload));
--- src/js/mapchip.js
+++ src/js/mapchip.js
@@ -1,46 +1,92 @@
 import {Observable} from './utils';
+import {map} from "leaflet/dist/leaflet-src.esm";
 
 let clonedMapChip;
+let clonedLegendBlock;
+let clonedLegend;
+
+const wrapperClsName = 'content__map_current-display';
 
 /**
  * Represent the map chip at the bottom of the map
  */
 export class MapChip extends Observable {
     constructor() {
-		super();
-		this.prepareDomElements();
+        super();
+        this.prepareDomElements();
     }
 
-	prepareDomElements() {
-        clonedMapChip = $('.chip--map')[0].cloneNode(true);
-		$(clonedMapChip).removeClass('hide');
+    prepareDomElements() {
+        clonedMapChip = $('.chip--map')[0].cloneNode(true);	//chip
+        clonedLegendBlock = $('.map_legend-block')[0].cloneNode(true);	//a legend block
+        clonedLegend = $('.content__map_legend')[0].cloneNode(true);	//the legend itself
+
+        $(clonedMapChip).removeClass('hide');
         this.clearAllMapChip();
     }
 
-	showMapChip() {
-		const element = $(clonedMapChip);
-		$('.content__map_current-display').append(element);
-		element.find('.chip__remove--map').on('click', () => this.removeMapChip(element));
-	}
+    showMapChip() {
+        const element = $(clonedMapChip);
 
-	clearAllMapChip() {
-        $('.content__map_current-display').html('');
+        $('.' + wrapperClsName).append(element);	//chip
+        element.find('.chip__remove--map').on('click', () => this.removeMapChip(element));
     }
 
-	removeMapChip(element) {
-		element.remove();
-		this.triggerEvent('mapChipRemoved', element);
-	}
+    showLegend(payload, mapLayers) {
+        if (payload.subindicators === null || typeof payload.subindicators === 'undefined' || payload.subindicators.length <= 0) {
+            return;
+        }
 
-	onSubIndicatorChange(payload) {
-		const label = `${payload.indicator} (${payload.obj.label})`
-		this.updateMapChipText(label);
-		this.showMapChip();
-	}
+        let subIndicators = payload.subindicators;
+        let children = Object.keys(subIndicators[0].children);
+        const legend = $(clonedLegend);
+        $('.' + wrapperClsName).append(legend);	    //legend
+        $('.' + wrapperClsName + ' .map_legend-block').remove(); //remove the previous legends
 
-	updateMapChipText(textValue) {
-		$(clonedMapChip).find( '.truncate' ).text(textValue);
-	}
+        let blockArr = [];
+
+        for (let i = 0; i < children.length; i++) {
+            let currentChild = mapLayers[children[i]];
+
+            const item = clonedLegendBlock.cloneNode(true);
+            const perc = parseFloat((currentChild.feature.properties.percentage * 100).toFixed(2));
+            const code = currentChild.feature.properties.code;
+            blockArr.push({
+                item: item,
+                perc: perc,
+                code: code,
+                fillColor: currentChild.options.fillColor
+            });
+        }
+
+        blockArr.sort((a, b) => (a.perc < b.perc) ? 1 : ((b.perc < a.perc) ? -1 : 0));
+
+        for (let i = 0; i < blockArr.length; i++) {
+            $(blockArr[i].item).css('background-color', blockArr[i].fillColor);
+            $('.truncate', blockArr[i].item).text(blockArr[i].code + ' - ' + blockArr[i].perc + '%');
+            $('.' + wrapperClsName + ' .content__map_legend').append(blockArr[i].item);
+        }
+    }
+
+    clearAllMapChip() {
+        $('.' + wrapperClsName).html('');
+    }
+
+    removeMapChip(element) {
+        element.remove();
+        this.triggerEvent('mapChipRemoved', element);
+    }
+
+    onSubIndicatorChange(payload, mapLayers) {
+        const label = `${payload.indicator} (${payload.obj.label})`
+        this.updateMapChipText(label);
+        this.showMapChip();
+        this.showLegend(payload, mapLayers);
+    }
+
+    updateMapChipText(textValue) {
+        $(clonedMapChip).find('.truncate').text(textValue);
+    }
 
 
 }

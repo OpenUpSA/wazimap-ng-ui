5b8cb89ed90ca79ac4ba3dc1d241c4a7f9dec32d
Adi Eyal <adi@openup.org.za>
2020-02-17 10:23:55 +0200

    Updated index.js to route to prod environment when required

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@ let baseUrl;
 
 if (window.location.href.search("wazi.webflow.io") >= 0)
     baseUrl = "https://wazimap-ng.openup.org.za";
+else if (window.location.href.search("openup.org.za/wazimap-ng-ui") >= 0)
+    baseUrl = "https://wazimap-ng.openup.org.za";
 else if (window.location.href.search("localhost") >= 0)
     baseUrl = "https://staging.wazimap-ng.openup.org.za"
 else

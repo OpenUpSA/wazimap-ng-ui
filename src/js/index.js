d705723315406ebac1c8d3fb5080071f557557be
Adi Eyal <adi@openup.org.za>
2020-03-05 16:21:49 +0200

    Added gcro routing

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,10 @@ else if (window.location.href.search("openup.org.za/wazimap-ng-ui") >= 0)
     baseUrl = "https://wazimap-ng.openup.org.za";
 else if (window.location.href.search("localhost") >= 0)
     baseUrl = "https://staging.wazimap-ng.openup.org.za"
+else if (window.location.href.search("gcro.openup.org.za") >= 0) {
+    baseUrl = "https://api.gcro.openup.org.za";
+    profileId = 1;
+}
 else
     baseUrl = "https://staging.wazimap-ng.openup.org.za"
 

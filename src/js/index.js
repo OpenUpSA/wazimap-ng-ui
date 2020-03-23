e119a5d3349729101c4895810d13e46508759349
Adi Eyal <adi@openup.org.za>
2020-03-23 10:19:20 +0200

    Refactored routing table

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,48 @@
-import configureApplication from "./load";
+import configureApplication from './load';
 import {geography_config} from './configurations/geography_sa';
 import {geography_config as gcro_config} from './configurations/geography_gcro';
 
-let profileId = 1;
-let baseUrl;
-let config = geography_config;
+const mainUrl = 'https://staging.wazimap-ng.openup.org.za';
 
-if (window.location.href.search("wazi.webflow.io") >= 0)
-    baseUrl = "https://wazimap-ng.openup.org.za";
-else if (window.location.href.search("openup.org.za/wazimap-ng-ui") >= 0)
-    baseUrl = "https://wazimap-ng.openup.org.za";
-else if (window.location.href.search("localhost") >= 0)
-    baseUrl = "https://staging.wazimap-ng.openup.org.za"
-else if (window.location.href.search("gcro.openup.org.za") >= 0) {
-    baseUrl = "https://api.gcro.openup.org.za";
-    config = gcro_config;
-    profileId = 1;
+const hostname = window.location.hostname;
+const profiles = {
+    'wazi.webflow.io': {
+        profile: 1,
+        baseUrl: mainUrl,
+        config: geography_config
+    },
+    'localhost': {
+        profile: 1,
+        baseUrl: mainUrl,
+        config: geography_config
+    },
+    'localhost-dev': {
+        profile: 1,
+        baseUrl: 'http://localhost:8000',
+        config: geography_config
+    },
+    'geo.vulekamali.gov.za': {
+        profile: 2,
+        baseUrl: mainUrl,
+        config: geography_config
+    },
+    'gcro.openup.org.za': {
+        profile: 1,
+        baseUrl: 'https://api.gcro.openup.org.za',
+        config: gcro_config
+    },
+    'staging.youthexplorer.org.za': {
+        profile: 1,
+        baseUrl: mainUrl,
+        config: geography_config
+    },
 }
-else if (window.location.href.search("geo.vulekamali.gov.za") >= 0) {
-    baseUrl = "https://staging.wazimap-ng.openup.org.za";
-    config = geography_config;
-    profileId = 2;
-}
-else
-    baseUrl = "https://staging.wazimap-ng.openup.org.za"
 
-configureApplication(baseUrl, profileId, config);
+const pc = profiles[hostname]
+if (pc != undefined) {
+    configureApplication(pc.baseUrl, pc.profile, pc.config);
+} else if (window.location.href.search('openup.org.za/wazimap-ng-ui') >= 0) {
+    configureApplication('https://wazimap-ng.openup.org.za', 1, geography_config);
+} else {
+    configureApplication(mainUrl, 1, geography_config);
+}

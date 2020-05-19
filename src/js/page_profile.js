7d1be0864566247337f3a2b6cc19db5ea782767d
Adi Eyal <adi@openup.org.za>
2020-05-19 21:37:09 +0200

    Revert "fixed the design change issue that caused key metrics not to show" Seems to have introduced a bug that prevents map navigation
    This reverts commit 908b36f41b920d36b16a0baec26cc64827275a07.

--- src/js/page_profile.js
+++ src/js/page_profile.js
@@ -133,14 +133,7 @@ export default class ProfileLoader {
     }
 
     addCategory(category, categoryDetail) {
-        const metricsArea = subcategoryTemplate.cloneNode(true);
-        $(metricsArea).find('.indicator__sub-indicator').remove();
-        $(metricsArea).find('.indicator__title').remove();
-        $(metricsArea).find('.indicator__description p').text(categoryDetail.description);
-        this.addKeyMetrics(metricsArea, categoryDetail);
-
         const newCategorySection = categoryTemplate.cloneNode(true);
-        $(newCategorySection).find('.data-category__header').after(metricsArea);
         const wrapper = newCategorySection;
 
         $(categoryHeaderTitleClass, newCategorySection).text(category);
@@ -179,7 +172,6 @@ export default class ProfileLoader {
         const chartContainer = $(chartContainerClass, subCategoryNode);
 
         this.addChart(chartContainer[0], subindicators);
-        //this.addKeyMetrics(chartContainer[0], detail.key_metrics);
 
         wrapper.append(subCategoryNode);
     }
@@ -275,23 +267,17 @@ export default class ProfileLoader {
         callback(selected);
     }
 
-    addKeyMetrics(container, categoryDetail) {
-        let key_metrics = [];
-        for (const [description, subcategories] of Object.entries(categoryDetail.subcategories)) {
-            key_metrics = key_metrics.concat(subcategories.key_metrics);
-        }
+    addKeyMetrics(container, key_metrics) {
 
         const wrapper = $(keyMetricWrapperClass, container)
         $(keyMetricClass, wrapper).remove()
 
         if (key_metrics != undefined && Array.isArray(key_metrics)) {
             key_metrics.forEach(el => {
-                if(typeof el !== 'undefined'){
-                    let metric = metricTemplate.cloneNode(true)
-                    $(".key-metric_value div", metric).text(el.value)
-                    $(".key-metric_title", metric).text(el.label)
-                    wrapper.append(metric)
-                }
+                let metric = metricTemplate.cloneNode(true)
+                $(".key-metric_value div", metric).text(el.value)
+                $(".key-metric_title", metric).text(el.label)
+                wrapper.append(metric)
             })
             // Show key metrics title
             wrapper.prev().css('display', 'block');
@@ -422,7 +408,7 @@ export default class ProfileLoader {
         const geometries = dataBundle.geometries;
 
         categoryTemplate = $(categoryClass)[0].cloneNode(true);
-        subcategoryTemplate = $(subcategoryClass, categoryTemplate)[0].cloneNode(true);
+        //subcategoryTemplate = $(subcategoryClass, categoryTemplate)[0].cloneNode(true);
         indicatorTemplate = $(indicatorClass, subcategoryTemplate)[0].cloneNode(true);
 
         $(categoryClass).addClass('hide');

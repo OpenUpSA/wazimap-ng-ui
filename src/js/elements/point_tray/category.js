35a04f0321afff174b40c934565968f22dc14d8d
emre2038 <emreyildirim2038@gmail.com>
2020-07-01 14:23:37 +0300

    coloring markers with point theme colors

--- src/js/elements/point_tray/category.js
+++ src/js/elements/point_tray/category.js
@@ -28,6 +28,8 @@ export class Category extends Observable {
             $(this.element).addClass('last');
         }
 
+        $(this.element).removeClass('theme-1').addClass('theme-' + this.theme.id);
+
         $('.point-mapper__h2_name .truncate', this.element).text(this.name);
         $('.point-data__label_source .truncate', this.element).text(this.data.metadata.source);
         $('.point-data__label_source .truncate', this.element).attr('title', this.data.metadata.source);

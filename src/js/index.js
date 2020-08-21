1626bc9929a21e2122bb322b1ef74f4d40c395cb
Adi Eyal <adi@openup.org.za>
2020-08-21 13:39:30 +0200

    Clean-up of index.js
    Removed hard-coded profile id and using the one return from the api instead

--- src/js/index.js
+++ src/js/index.js
@@ -21,83 +21,67 @@ const defaultConfig = new SAConfig();
 
 const profiles = {
     'wazi.webflow.io': {
-        profile: defaultProfile,
         baseUrl: mainUrl,
         config: config
     },
     'localhost': {
-        profile: 1,
         baseUrl: mainUrl,
         //baseUrl: productionUrl,
         config: config
     },
     'localhost-dev': {
-        profile: 1,
         baseUrl: 'http://localhost:8000',
         config: config
     },
     'geo.vulekamali.gov.za': {
-        profile: 2,
         baseUrl: productionUrl,
         config: config
     },
     'gcro.openup.org.za': {
-        profile: 1,
         baseUrl: 'https://api.gcro.openup.org.za',
         config: new GCROConfig()
     },
     'beta.youthexplorer.org.za': {
-        profile: 8,
         baseUrl: productionUrl,
         config: config
     },
     'capetownagainstcovid19.openup.org.za': {
-        profile: 3,
         baseUrl: mainUrl,
         config: config
     },
     'covid-wazi.openup.org.za': {
-        profile: 4,
         baseUrl: productionUrl,
         config: config
     },
     'wazimap-ng.africa': {
-        profile: 1,
         baseUrl: mainUrl,
         config: config
     },
     'covid-ibp.openup.org.za': {
-        profile: 5,
         baseUrl: productionUrl,
         config: config
     },
     'sifar-wazi.openup.org.za': {
-        profile: 6,
         baseUrl: productionUrl,
         config: config
     },
     'mapyourcity.org.za': {
-        profile: 7,
         baseUrl: productionUrl,
         config: config
     },
     'giz-projects.openup.org.za': {
-        profile: 11,
         baseUrl: productionUrl,
         config: config
     },
     'covid-ccij.openup.org.za': {
-        profile: 5,
         baseUrl: mainUrl,
         config: new WorldConfig()
     },
     'cfafrica.wazimap-ng.africa': {
-        profile: 8,
         baseUrl: 'https://api.cfafrica.wazimap-ng.africa',
         config: config
     },
     'ccij-water.openup.org.za': {
-        profile: 16,
         baseUrl: productionUrl,
         config: new WorldConfig()
     }
@@ -117,6 +101,7 @@ async function init() {
 
     pc.config.setConfig(data.configuration || {})
     pc.config.api = api;
+    pc.profile = data.id;
     pc.config.baseUrl = pc.baseUrl;
     // TODO add this to config - check the <script> tag in the HTML which hardcodes this value
     pc.config.analytics = new Analytics('UA-93649482-25', pc.profile);

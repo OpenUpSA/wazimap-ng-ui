9baac76d9ea0e3f35cb5be07087123b344491aa7
Adi Eyal <adi@openup.org.za>
2020-09-05 21:36:02 +0200

    Pulling hostname from window.location

--- src/js/index.js
+++ src/js/index.js
@@ -13,8 +13,7 @@ const mainUrl = 'https://staging.wazimap-ng.openup.org.za';
 const productionUrl = 'https://production.wazimap-ng.openup.org.za';
 const config = new SAConfig();
 
-// const hostname = window.location.hostname;
-const hostname = "beta.youthexplorer.org.za"
+const hostname = window.location.hostname;
 const defaultProfile = 8;
 const defaultUrl = productionUrl;
 const defaultConfig = new SAConfig();

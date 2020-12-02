b3315ccc47872d98bb681f2e7f11dd2964b75246
Makar Kozlowski <makar.kozlowski.mak@gmail.com>
2020-12-02 21:51:42 +0100

    feat: add the choropleth legend onto the map download

--- src/js/map/map_download.js
+++ src/js/map/map_download.js
@@ -21,10 +21,18 @@ export class MapDownload extends Observable {
         };
         const title = $(`<div id="map-download-title">${this.mapChip.title}</div>`);
         const element = document.getElementById("main-map");
+
+        const legend = document.querySelector('.map-options__legend');
+        let clonedLegend = legend.cloneNode(true);
+        $(clonedLegend).find('.map-options__legend_label').remove();
+        clonedLegend.id = 'map-download-legend';
+
         $(element).append(title);
+        $(element).append(clonedLegend);
 
         html2canvas(element, options).then(function (canvas) {
             $(title).remove();
+            $(clonedLegend).remove();
             saveAs(canvas.toDataURL(), 'map.png');
         });
     }

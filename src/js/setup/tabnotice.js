70d7c9d7729d5ac1a67409ec78284ae62ebaeeaa
Mila Frerichs <mila.frerichs@gmail.com>
2021-02-25 10:48:10 +0100

    remove param
    not needed anymore

--- src/js/setup/tabnotice.js
+++ src/js/setup/tabnotice.js
@@ -1,3 +1,3 @@
 export function configureTabNoticeEvents(controller, tabNotice) {
-    controller.on('profile.loaded', payload => tabNotice.modifyTabNotice(payload.state.profile.profile))
+    controller.on('profile.loaded', payload => tabNotice.modifyTabNotice())
 }

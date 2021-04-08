d731fe6e4cf4f2917323aef3f03d0f8574b9d3ca
emre2038 <emreyildirim2038@gmail.com>
2021-04-08 15:32:55 +0300

    sibling calculator

--- src/js/map/choropleth/sibling_calculator.js
+++ src/js/map/choropleth/sibling_calculator.js
@@ -1,17 +1,23 @@
 import {percFmt, numFmt} from '../../utils'
 
 export default class SiblingCalculator {
-    calculate(args) {
-        if (typeof args.data === 'undefined'){
-            return null;
-        }
+    calculate(childData, primaryGroup, selectedSubindicator) {
+        let sumData = {};
+        Object.entries(childData).map(([code, data]) => {
+            let filteredArr = data.filter((a) => {
+                return a[primaryGroup] === selectedSubindicator;
+            });
 
-        const universe = Object.entries(args.data).reduce((total, c) => {
+            sumData[code] = filteredArr.reduce(function (s, a) {
+                return s + parseFloat(a.count);
+            }, 0);
+        })
+
+        const universe = Object.entries(sumData).reduce((total, c) => {
             return total + c[1];
         }, 0)
 
-
-        const result = Object.entries(args.data).map(childGeography => {
+        const result = Object.entries(sumData).map(childGeography => {
             const code = childGeography[0];
             const count = childGeography[1];
             const val = count / universe;

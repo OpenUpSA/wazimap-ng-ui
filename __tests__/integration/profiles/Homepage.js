b4346b9621560311711bb663226589bafa9da2bd
Mila Frerichs <mila.frerichs@gmail.com>
2021-04-23 10:20:18 +0200

    chore: increase timeouts to make sure this works
    loading profiles can take longer, so try out if the increase in timeout
    helps with that.
    might want to increase it globally until we work on the loading times

--- __tests__/integration/profiles/Homepage.js
+++ __tests__/integration/profiles/Homepage.js
@@ -1,10 +1,10 @@
 import { When, Given, Then } from "cypress-cucumber-preprocessor/steps";
 
 Then('I should see a Geography in the breadcrumps', () => {
-  cy.get(".map-location__tags .location-tag__name").should('not.contain', 'Loading...')
+  cy.get(".map-location__tags .location-tag__name", { timeout: 100000 }).should('not.contain', 'Loading...')
 })
 
 Then('I should see a title in the Rich Data View', () => {
-  cy.get(".location__title h1").should('not.be.empty')
+  cy.get(".location__title h1", { timeout: 100000 }).should('not.be.empty')
 })
 

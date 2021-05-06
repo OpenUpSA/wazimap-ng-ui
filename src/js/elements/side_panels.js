78c82e4e8872ce7499a564bfbdd250236a5654c2
emre2038 <emreyildirim2038@gmail.com>
2021-05-07 00:38:17 +0300

    removed unnecessary logging

--- src/js/elements/side_panels.js
+++ src/js/elements/side_panels.js
@@ -99,7 +99,6 @@ export class SidePanels extends Observable {
 
     initialiseBlankTriggers = () => {
         this.emptyPanel.richData.click(() => {
-            console.log('initialiseBlankTriggers');
             this.triggerEvent('panel.rich_data.opened');
         })
 
@@ -113,7 +112,6 @@ export class SidePanels extends Observable {
     }
 
     toggleRichData = () => {
-        console.log('toggleRichData')
         this.emptyPanel.richData.click();
     }
 

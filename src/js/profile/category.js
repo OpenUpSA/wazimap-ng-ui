b93b9575f131b95a237c832c1bfded49d5c7b09b
emre2038 <emreyildirim2038@gmail.com>
2021-06-02 16:14:31 +0300

    appending an error message if there is no data to display on the chart

--- src/js/profile/category.js
+++ src/js/profile/category.js
@@ -72,15 +72,13 @@ export class Category extends Observable {
         let lastIndex = Object.entries(detail.subcategories).length - 1;
         for (const [subcategory, detail] of Object.entries(detail.subcategories)) {
             let hasChildren = checkIfSubCategoryHasChildren(subcategory, detail);
-            if (hasChildren) {
-                let isFirst = index === 0;
-                let sc = new Subcategory(this.formattingConfig, wrapper, subcategory, detail, isFirst);
-                this.bubbleEvents(sc, [
-                    'profile.chart.saveAsPng', 'profile.chart.valueTypeChanged',
-                    'profile.chart.download_csv', 'profile.chart.download_excel', 'profile.chart.download_json', 'profile.chart.download_kml',
-                    'point_tray.subindicator_filter.filter'
-                ]);
-            }
+            let isFirst = index === 0;
+            let sc = new Subcategory(this.formattingConfig, wrapper, subcategory, detail, isFirst);
+            this.bubbleEvents(sc, [
+                'profile.chart.saveAsPng', 'profile.chart.valueTypeChanged',
+                'profile.chart.download_csv', 'profile.chart.download_excel', 'profile.chart.download_json', 'profile.chart.download_kml',
+                'point_tray.subindicator_filter.filter'
+            ]);
             index++;
         }
     }

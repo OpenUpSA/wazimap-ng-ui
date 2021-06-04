af6a5d0c0f3c9e6a630a086d8ef1acc845f73b12
emre2038 <emreyildirim2038@gmail.com>
2021-06-04 15:11:14 +0300

    triggering click events of dropdowns

--- src/js/profile/subindicator_filter.js
+++ src/js/profile/subindicator_filter.js
@@ -46,7 +46,9 @@ export class SubindicatorFilter extends Observable {
     let groupCallback = (selected) => this.groupSelected(selected, subindicatorDd, title);
 
     this.setOptionSelected(indicatorDd, this.selectedGroup, groupCallback);
+    $(indicatorDd).find('.dropdown__list_item.selected').click();
     this.setOptionSelected(subindicatorDd, selectedFilter, this.handleFilter);
+    $(subindicatorDd).find('.dropdown__list_item.selected').click();
 
   }
 
@@ -113,7 +115,7 @@ export class SubindicatorFilter extends Observable {
         if (i !== 0 && $(li).hasClass('selected')) {
           //leave the first item selected in default
           //                    $(li).removeClass('selected')
-          //                                    
+          //
         }
         if (typeof item.name === 'undefined') {
           $('.truncate', li).text(item);

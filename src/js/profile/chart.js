a0833b0a4dbe2be8bf185bb303db8be22d884d4a
Warachi <morris.warachi@gro-intelligence.com>
2021-06-08 10:41:16 +0300

    fix: download chart

--- src/js/profile/chart.js
+++ src/js/profile/chart.js
@@ -119,6 +119,7 @@ export class Chart extends Observable {
         this.vegaView = result.view;
         this.setChartMenu();
         this.showChartDataTable();
+        this.setDownloadUrl()
         let $svg = $(this.container).find('svg')
         $svg.attr('preserveAspectRatio', 'xMinYMin meet')
         $svg.removeAttr('width')
@@ -233,7 +234,7 @@ export class Chart extends Observable {
     }
   }
 
-  setChartMenu = (barChart) => {
+  setChartMenu = (barChart`) => {
     const self = this;
     const saveImgButton = $(this.containerParent).find('.hover-menu__content a.hover-menu__content_item:nth-child(1)');
 

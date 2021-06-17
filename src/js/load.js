3fff577867c93d264f57312f3f5f442dc0a55253
Adi Eyal <adi@openup.org.za>
2021-06-17 16:27:16 +0200

    Minor bug fixes

--- src/js/load.js
+++ src/js/load.js
@@ -79,7 +79,7 @@ class Application extends Component {
         const tutorial = new Tutorial(this);
 		const styleConfig = new StyleConfig(config.style);
         const tabNotice = new TabNotice(this, config.config.feedback);
-        const translations = new Translations(config.config.translations);
+        const translations = new Translations(this, config.config.translations);
         const dataMapperMenu = new DataMapperMenu(this);
 
         configureMapEvents(controller, {mapcontrol: mapcontrol, zoomToggle: zoomToggle});

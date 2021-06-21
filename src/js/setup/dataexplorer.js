6f70e0544d086963d21d83badc231b8d84abfdd5
Warachi <morris.warachi@gro-intelligence.com>
2021-06-21 15:18:44 +0300

    use correct component import

--- src/js/setup/dataexplorer.js
+++ src/js/setup/dataexplorer.js
@@ -1,16 +1,17 @@
-import {LoadMenu, showNoData, DataMenu} from '../elements/menu';
+import { LoadMenu, showNoData, DataMapperMenu } from '../elements/menu';
 
 export function configureDataExplorerEvents(controller, dataMapperMenu) {
-    controller.bubbleEvent(dataMapperMenu, 'data_mapper_menu.nodata')
-    controller.on('profile.loaded', payload => {
-        if ($.isEmptyObject(payload.payload.geometries.children)) {
-            //no children -- show no-data chip
-            dataMenu.showNoData();
-        } else {
-            const profileData = payload.payload.profile.profileData;
-            new LoadMenu(dataMapperMenu, profileData, payload=> {
-              controller.onSubIndicatorClick(payload)
-          });
-        }
-    })
+  controller.bubbleEvent(dataMapperMenu, 'data_mapper_menu.nodata')
+  controller.on('profile.loaded', payload => {
+    const loadMenu = new DataMapperMenu(dataMapperMenu);
+    if ($.isEmptyObject(payload.payload.geometries.children)) {
+      //no children -- show no-data chip
+      loadMenu.showNoData();
+    } else {
+      const profileData = payload.payload.profile.profileData;
+      loadMenu.init(profileData, payload => {
+        controller.onSubIndicatorClick(payload)
+      });
+    }
+  });
 }

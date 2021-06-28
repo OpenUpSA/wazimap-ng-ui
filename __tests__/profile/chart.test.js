8e3a66eb7911a0da04e3329e3b6a9ce55a4e3434
michaelglenister <mickeyjhg@gmail.com>
2021-06-28 15:56:23 +0200

    Add initial test file

---
+++ __tests__/profile/chart.test.js
@@ -0,0 +1,7 @@
+import {Chart} from "../../src/js/profile/chart";
+import {screen} from "@testing-library/dom";
+import {Component} from '../../src/js/utils';
+
+const PERCENTAGE_TYPE = "percentage";
+const VALUE_TYPE = "value";
+

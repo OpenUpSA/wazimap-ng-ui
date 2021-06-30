ca8e048cfb019b506ae6ea78be4745da9e197831
Warachi <morris.warachi@gro-intelligence.com>
2021-06-30 21:52:35 +0300

    rename vars

--- src/js/setup/dataexplorer.js
+++ src/js/setup/dataexplorer.js
@@ -3,13 +3,13 @@ import { DataMapperMenu } from '../elements/menu';
 export function configureDataExplorerEvents(controller, dataMapperMenu) {
   controller.bubbleEvent(dataMapperMenu, 'data_mapper_menu.nodata')
   controller.on('profile.loaded', payload => {
-    const loadMenu = new DataMapperMenu(dataMapperMenu);
+    const dataMenu = new DataMapperMenu(dataMapperMenu);
     if ($.isEmptyObject(payload.payload.geometries.children)) {
       //no children -- show no-data chip
-      loadMenu.showNoDataMessage();
+      dataMenu.showNoDataMessage();
     } else {
       const profileData = payload.payload.profile.profileData;
-      loadMenu.init(profileData, payload => {
+      dataMenu.init(profileData, payload => {
         controller.onSubIndicatorClick(payload)
       });
     }

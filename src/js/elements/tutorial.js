6d0e4c8ee8ae726242c4c61f5f9bf51c29c3ec65
emre2038 <emreyildirim2038@gmail.com>
2021-07-20 22:50:44 +0300

    fixing tutorials and logging error if the element is not found

--- src/js/elements/tutorial.js
+++ src/js/elements/tutorial.js
@@ -13,9 +13,18 @@ export class Tutorial extends Component {
 
         tutorials.forEach((slide, i) => {
             let item = $(this.wrapperClass).find(this.slideClass)[i];
-            $('.slide-info__title', item).text(slide.title);
-            $('.slide-info__introduction', item).text(slide.body);
+            this.setTextOrLogError('.slide-info__title', item, slide.title, i);
+            this.setTextOrLogError('.slide-info__introduction', item, slide.body, i);
             $('.tutorial__slide-image', item).css('background-image', `url(${slide.image})`);
         });
+    }
+
+    setTextOrLogError = (cls, item, text, index) => {
+        let ele = $(cls, item);
+        if (ele.length <= 0) {
+            console.error(`Missing html element in slide ${index + 1} : ${cls}`);
+            return;
+        }
+        ele.text(text);
     }
 }

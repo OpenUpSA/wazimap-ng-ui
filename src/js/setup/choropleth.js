90f21a16e9c177112b8ac0f884b47f134994c39d
Adi Eyal <adi@clearforest.io>
2021-08-02 14:16:08 +0200

    Revert "Merge remote-tracking branch 'origin/staging' into refactor-filter-code"
    This reverts commit 8ec49e5b786604e9ca8ae010b437594b071655a2, reversing
    changes made to 33cb22c55504dd2575d45cfbc9090eb087caa9dc.

--- src/js/setup/choropleth.js
+++ src/js/setup/choropleth.js
@@ -38,7 +38,18 @@ export function configureChoroplethEvents(controller, objs = {mapcontrol: null, 
             return;
         }
 
-        mapchip.onSubIndicatorChange(args);
+        const metadata = args.data.metadata;
+        const params = {
+            primaryGroup: metadata.primary_group,
+            groups: metadata.groups,
+            indicatorTitle: args.indicatorTitle,
+            selectedSubindicator: args.selectedSubindicator,
+            childData: args.data.child_data,
+            description: args.description,
+            chartConfiguration: args.data.chartConfiguration,
+            filter: args.filter,
+        }
+        mapchip.onSubIndicatorChange(params);
     });
 
     controller.on('redraw', payload => {
@@ -58,7 +69,7 @@ function loadAndDisplayChoropleth(payload, mapcontrol, showMapchip = false, chil
     const filter = ps.subindicator.filter;
     let data = ps.subindicator.data
     if (childData) {
-        data.originalChildData = (typeof data.originalChildData !== 'undefined' && data.originalChildData !== null) ? data.originalChildData : data.child_data;
+        data.originalChildData = (data.originalChildData !== undefined) ? data.originalChildData : data.child_data;
         data.child_data = childData;
     }
     mapcontrol.handleChoropleth(data, method, selectedSubindicator, indicatorTitle, showMapchip, filter);

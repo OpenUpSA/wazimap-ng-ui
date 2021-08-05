8935d0d671a7a827b0583f201d3d73236edb9b51
emre2038 <emreyildirim2038@gmail.com>
2021-08-05 14:27:44 +0300

    fixed test

--- __tests__/elements/side_panels.test.js
+++ __tests__/elements/side_panels.test.js
@@ -1,14 +1,22 @@
-import { screen, fireEvent, getByText } from '@testing-library/dom'
+import {screen, fireEvent, getByText} from '@testing-library/dom'
 
-import { SidePanels } from "../../src/js/elements/side_panels.js";
+import {SidePanels} from "../../src/js/elements/side_panels.js";
 import {Component} from '../../src/js/utils';
+import Controller from "../../src/js/controller";
+import {MapDownload} from "../../src/js/map/map_download";
+import {configureMapDownloadEvents} from "../../src/js/setup/mapdownload";
+import {configureMiscElementEvents} from "../../src/js/setup/miscelements";
+import {Config as SAConfig} from "../../src/js/configurations/geography_sa";
 
 describe('SidePanels', () => {
-  let sp;
-  let triggerEvent;
-  let showPanels = { 'rich_data': true, 'point_data': true, 'data_explorer': true }
-  beforeEach(() => {
-    document.body.innerHTML = `
+    let sp;
+    let triggerEvent;
+    let showPanels = {'rich_data': true, 'point_data': true, 'data_explorer': true};
+    let config = new SAConfig();
+    const controller = new Controller(this, null, config, 1);
+    const mapDownload = new MapDownload(this, null);
+    beforeEach(() => {
+        document.body.innerHTML = `
     <div class="rich-data-toggles">
       <div class="panel-toggle" data-testid="rich-data-rich"></div>
       <div class="panel-toggle" data-testid="rich-data-point"></div>
@@ -30,38 +38,41 @@ describe('SidePanels', () => {
       <div class="panel-toggle" data-testid="empty-map"></div>
     </div>
     <div class="map-download hover-text-primary disabled"></div>
+    <div class="location-tag__loading-icon"></div>
     `;
-  })
-  describe('click interactions', () => {
-    describe('just close the panel', () => {
-      test('calls close event', () => {
-        let component = new Component();
-        sp = new SidePanels(component, showPanels)
-        triggerEvent = jest.spyOn(sp, 'triggerEvent')
+    })
+    describe('click interactions', () => {
+        describe('just close the panel', () => {
+            test('calls close event', () => {
+                configureMiscElementEvents(this, controller);
+                configureMapDownloadEvents(controller, mapDownload);
+                let component = new Component();
+                sp = new SidePanels(component, showPanels)
+                triggerEvent = jest.spyOn(sp, 'triggerEvent')
 
-        fireEvent.click(screen.getByTestId('rich-data-rich'));
+                fireEvent.click(screen.getByTestId('rich-data-rich'));
 
-        expect(triggerEvent).toBeCalledTimes(1);
-        expect(triggerEvent).toHaveBeenNthCalledWith(1, 'panel.rich_data.closed');
+                expect(triggerEvent).toBeCalledTimes(1);
+                expect(triggerEvent).toHaveBeenNthCalledWith(1, 'panel.rich_data.closed');
 
-        let mapDownloadButton = document.querySelector(".map-download");
-        expect(mapDownloadButton).not.toHaveClass('disabled');
-      });
-      })
-    describe('open a new panel', () => {
-      test('call close and open event', () => {
-        let component = new Component();
-        sp = new SidePanels(component, showPanels)
-        triggerEvent = jest.spyOn(sp, 'triggerEvent')
+                let mapDownloadButton = document.querySelector(".map-download");
+                expect(mapDownloadButton).not.toHaveClass('disabled');
+            });
+        })
+        describe('open a new panel', () => {
+            test('call close and open event', () => {
+                let component = new Component();
+                sp = new SidePanels(component, showPanels)
+                triggerEvent = jest.spyOn(sp, 'triggerEvent')
 
-        fireEvent.click(screen.getByTestId('rich-data-point'));
+                fireEvent.click(screen.getByTestId('rich-data-point'));
 
-        expect(triggerEvent).toBeCalledTimes(2);
-        expect(triggerEvent).toHaveBeenNthCalledWith(1, 'panel.rich_data.closed');
-        expect(triggerEvent).toHaveBeenNthCalledWith(2, 'panel.point_mapper.opened');
-      });
-    });
+                expect(triggerEvent).toBeCalledTimes(2);
+                expect(triggerEvent).toHaveBeenNthCalledWith(1, 'panel.rich_data.closed');
+                expect(triggerEvent).toHaveBeenNthCalledWith(2, 'panel.point_mapper.opened');
+            });
+        });
 
-  })
+    })
 })
 

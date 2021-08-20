cae537b7e9963d3c0cd65f4d1a4649d4a7deb48d
michaelglenister <mickeyjhg@gmail.com>
2021-08-20 12:14:42 +0200

    Mock controller of the datamapper menu

--- __tests__/profile/data_mapper.test.js
+++ __tests__/profile/data_mapper.test.js
@@ -1,7 +1,8 @@
 import {screen, fireEvent, getByText} from '@testing-library/dom'
-import {loadMenu} from '../../src/js/elements/menu';
+import Controller from '../../src/js/controller';
 import {Component} from '../../src/js/utils';
 import html from '../../src/index.html';
+import {loadMenu} from '../../src/js/elements/menu';
 
 describe('Data mapper panel tests', () => {
     document.body.innerHTML = html;
@@ -32,7 +33,33 @@ describe('Data mapper panel tests', () => {
         }
     }
 
-    let menu = loadMenu("", MenuData, "")
+    const config = {
+        "config": {
+            "urls": [
+                "localhost"
+            ]
+        },
+        "api": {
+            "eventListeners": {},
+            "token": null,
+            "baseUrl": "http://127.0.0.1:8000/api/v1",
+            "busyLoggingIn": false,
+            "failedLogins": 0
+        },
+        "baseUrl": "http://127.0.0.1:8000",
+        "analytics": {
+            "timer": {
+                "timers": {}
+            },
+            "profileId": 1
+        },
+        "profile": 1
+    }
+    const api = config.api;
+    const profileId = 1
+
+    const controller = new Controller(this, api, config, profileId);
+    let menu = loadMenu(controller, MenuData, "")
 
     test('Check that qualitative indicators are not populated in the datamapper menu', () => {
         let descriptionElement = document.querySelector(".data-category__h3");

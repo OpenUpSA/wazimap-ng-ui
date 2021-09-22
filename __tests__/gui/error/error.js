4f2851d652edaedf6fa786e910a8a35e0d76a1df
JD Bothma <jbothma@gmail.com>
2021-09-22 13:13:58 +0200

    Update __tests__/gui/error/error.js

--- __tests__/gui/error/error.js
+++ __tests__/gui/error/error.js
@@ -8,6 +8,6 @@ Given('I intercept the requests and respond null', () => {
 
 Then('An alert message should be shown', () => {
     cy.on('window:alert', (str) => {
-        expect(str).to.equal(`An unexpected error occurred. Please reload the page and try again. If the problem persists, please contact wazimap-support@openup.org.za`)
+        expect(str).to.equal(`An unexpected error occurred. Please reload the page and try again. If the problem persists, please contact support@wazimap.co.za`)
     })
 })

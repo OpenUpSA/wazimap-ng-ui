2a59dd129b3d28bead7f7b685bd699fdfa32d9ae
emre2038 <emreyildirim2038@gmail.com>
2021-10-12 22:33:07 +0300

    gui test

--- __tests__/gui/multi_boundary_versions.feature
+++ __tests__/gui/multi_boundary_versions.feature
@@ -3,3 +3,12 @@ Feature: Multiple geographic boundary versions
   Scenario: Verify that the map works correctly with multiple boundary versions
     Given I am on the Wazimap Homepage
       Then I wait until map is ready
+    And I check if "2011 Boundaries" is the selected version
+      Then I switch to "2016 with wards" version
+    And I click on the Proceed button in confirmation modal
+      Then I check if "2016 with wards" is the selected version
+    And I expand Data Mapper
+      Then I check if there are 2 categories
+    And I select an indicator from Elections category
+      Then I click on the Proceed button in confirmation modal
+    And I check if "2011 Boundaries" is the selected version
--- __tests__/gui/multi_boundary_versions/multi_boundary_versions.js
+++ __tests__/gui/multi_boundary_versions/multi_boundary_versions.js
@@ -1,4 +1,4 @@
-import {Given, Then} from "cypress-cucumber-preprocessor/steps";
+import {Given, Then, When} from "cypress-cucumber-preprocessor/steps";
 import {gotoHomepage, setupInterceptions, waitUntilGeographyIsLoaded} from "../common_cy_functions/general";
 import profiles from "../multi_boundary_versions/profiles.json";
 import all_details_2011 from "../multi_boundary_versions/all_details_2011.json";
@@ -28,4 +28,34 @@ Given('I am on the Wazimap Homepage', () => {
 
 Then('I wait until map is ready', () => {
     waitUntilGeographyIsLoaded('Version Test');
+})
+
+Then(/^I check if "([^"]*)" is the selected version$/, (versionName) => {
+    cy.get('.map-geo-select').should('be.visible');
+    cy.get('.map-geo-select .dropdown-menu .dropdown-menu__selected-item .truncate').should('contain.text', versionName);
+});
+
+Then(/^I switch to "([^"]*)" version$/, (versionName) => {
+    cy.get('.dropdown-menu').click();
+    cy.get(`.dropdown-menu__content:visible .dropdown__list_item:visible:contains("${versionName}")`).click();
+});
+
+Then('I click on the Proceed button in confirmation modal', () => {
+    cy.get('.warning-modal').should('be.visible');
+    cy.get('#warning-proceed').click();
+})
+
+When('I expand Data Mapper', () => {
+    cy.get('.point-mapper-toggles .data-mapper-panel__open').click();
+})
+
+Then('I check if there are 2 categories', () => {
+    cy.get('.data-mapper-content__list .data-category').should('have.length', 2);
+})
+
+When('I select an indicator from Elections category', ()=>{
+    cy.get('.data-category__h1_title').contains('Elections').click();
+    cy.get('.data-category__h1_content').contains('2011 Municipal elections').click();
+    cy.get('.data-category__h2_content').contains('Voter turnout').click();
+    cy.get('.data-category__h3_content').contains('Voted').click();
 })

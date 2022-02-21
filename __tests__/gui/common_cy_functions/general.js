dad13196f153ba6bfef1f0b45f3b2e6c5eb256e7
emre2038 <emreyildirim2038@gmail.com>
2022-02-21 16:55:10 +0300

    testing legend visibility

--- __tests__/gui/common_cy_functions/general.js
+++ __tests__/gui/common_cy_functions/general.js
@@ -179,8 +179,10 @@ export function collapseChoroplethFilterDialog() {
 
 export function checkIfChoroplethFilterDialogIsCollapsed() {
     const descriptionArea = '.map-options__context--v2';
+    const legendArea = '.map-options__legend';
     checkIfFilterDialogIsCollapsed('.map-options', '.map-options__filters_content');
     cy.get(`${mapBottomItems} .map-options ${descriptionArea}`).should('not.be.visible');
+    cy.get(`${mapBottomItems} .map-options ${legendArea}`).should('be.visible');
 }
 
 export function expandChoroplethFilterDialog() {
@@ -189,8 +191,10 @@ export function expandChoroplethFilterDialog() {
 
 export function checkIfChoroplethFilterDialogIsExpanded() {
     const descriptionArea = '.map-options__context--v2';
+    const legendArea = '.map-options__legend';
     checkIfFilterDialogIsExpanded('.map-options', '.map-options__filters_content');
     cy.get(`${mapBottomItems} .map-options ${descriptionArea}`).should('be.visible');
+    cy.get(`${mapBottomItems} .map-options ${legendArea}`).should('be.visible');
 }
 
 function expandFilterDialog(parentDiv) {

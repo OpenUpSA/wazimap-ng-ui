a7b81637b1ba377f991f6e929fef195fa067a1e1
Gaurav <goyal1092@gmail.com>
2022-02-25 07:57:59 +0530

    Fixed breaking test and added a test for behaviour of vega in case of missing data

--- __tests__/profile/charts/barchart.test.js
+++ __tests__/profile/charts/barchart.test.js
@@ -214,7 +214,7 @@ describe('Test downloadable barchart', () => {
             'attribution': 'Profile config attribution',
             'graphValueType': 'Percentage'
         }
-        let vegaDownloadSpec = configureBarchartDownload(data, metadata, config, annotations);
+        let vegaDownloadSpec = configureBarchartDownload(data, data, metadata, config, annotations);
         let view = renderVegaHeadless(vegaDownloadSpec);
         await view.runAsync()
 
@@ -234,7 +234,7 @@ describe('Test downloadable barchart', () => {
             'graphValueType': 'Percentage'
         }
         metadata["source"] = '';
-        let vegaDownloadSpec = configureBarchartDownload(data, metadata, config, annotations);
+        let vegaDownloadSpec = configureBarchartDownload(data, data, metadata, config, annotations);
         let view = renderVegaHeadless(vegaDownloadSpec);
         await view.runAsync()
         expect(view.signal('source').toString()).toBe('');
@@ -317,7 +317,7 @@ describe('Test downloadable barchart', () => {
           'attribution': 'Profile config attribution',
           'graphValueType': 'Percentage'
       }
-      let vegaDownloadSpec = configureBarchartDownload([], metadata, config, annotations);
+      let vegaDownloadSpec = configureBarchartDownload([], [], metadata, config, annotations);
       let view = renderVegaHeadless(vegaDownloadSpec);
       await view.runAsync()
 
@@ -335,7 +335,7 @@ describe('Test downloadable barchart', () => {
           'graphValueType': 'Percentage'
       }
       metadata["source"] = null;
-      let vegaDownloadSpec = configureBarchartDownload([], metadata, config, annotations);
+      let vegaDownloadSpec = configureBarchartDownload([], [], metadata, config, annotations);
       let view = renderVegaHeadless(vegaDownloadSpec);
       await view.runAsync()
       expect(view.signal('source')).toBe('');
@@ -349,7 +349,7 @@ describe('Test downloadable barchart', () => {
           'graphValueType': 'Percentage'
       }
       metadata["source"] = undefined;
-      let vegaDownloadSpec = configureBarchartDownload([], metadata, config, annotations);
+      let vegaDownloadSpec = configureBarchartDownload([], [], metadata, config, annotations);
       let view = renderVegaHeadless(vegaDownloadSpec);
       await view.runAsync()
       expect(view.signal('source')).toBe('');
@@ -367,9 +367,79 @@ describe('Test downloadable barchart', () => {
         primary_group: 'age',
         groups: [ { name: 'age' } ]
       }
-      let vegaDownloadSpec = configureBarchartDownload([], metadata, config, annotations);
+      let vegaDownloadSpec = configureBarchartDownload([], [], metadata, config, annotations);
       let view = renderVegaHeadless(vegaDownloadSpec);
       await view.runAsync()
       expect(view.signal('source')).toBe('Source : test');
     });
+});
+
+
+describe('configureBarchart for missing data', () => {
+    document.body.innerHTML = html;
+
+    let data, metadata, config;
+    beforeEach(() => {
+        metadata = {
+            primary_group: "education_level",
+            groups: [{name: "education_level"}, {name: "gender"}]
+        },
+            data = [
+                {age: 15, gender: 'male', count: 1, education_level: "Grade9"},
+                {age: 12, gender: 'female', count: 3, education_level: "Grade10"},
+                {age: 14, gender: 'male', count: 2, education_level: "Grade9"}
+            ]
+        config = {
+            types: {
+                Value: {
+                    formatting: ",.0f",
+                    minX: "default",
+                    maxX: "default"
+                },
+                Percentage: {
+                    formatting: ".0%",
+                    minX: "default",
+                    maxX: "default"
+                }
+            },
+            disableToggle: false,
+            defaultType: "Value",
+            xTicks: null
+        }
+    });
+
+    test('Test missing data after apply filters', async () => {
+      let vegaSpec = configureBarchart(data, metadata, config);
+      let view = renderVegaHeadless(vegaSpec);
+      await view.runAsync()
+
+      let yscaleDomain = view._runtime.scales.yscale._argval.domain;
+      // assert there will be 2 domains - Grade9 & Grade10
+      expect(yscaleDomain.length).toEqual(2);
+      expect(yscaleDomain).toEqual(["Grade9", "Grade10"]);
+
+      // Assert there will be 2 bars for 2 domains
+      let bars = view._runtime.data.bars.values.value;
+      expect(bars.length).toEqual(2);
+      expect(bars[0].datum.education_level).toEqual("Grade9");
+      expect(bars[1].datum.education_level).toEqual("Grade10");
+
+      // Call Filter Signal
+      view.signal('genderFilter', true).run();
+      view.signal('genderFilterValue', "male").run();
+
+      await view.runAsync();
+      yscaleDomain = view._runtime.scales.yscale._argval.domain;
+
+      // assert there will be 2 domains - Grade9 & Grade10
+      expect(yscaleDomain.length).toEqual(2);
+      expect(yscaleDomain).toEqual(["Grade9", "Grade10"]);
+
+      // Assert there will be 1 bar for 2 domains
+      bars = view._runtime.data.bars.values.value;
+      expect(bars.length).toEqual(1);
+      expect(bars[0].datum.education_level).toEqual("Grade9");
+
+    });
+
 });

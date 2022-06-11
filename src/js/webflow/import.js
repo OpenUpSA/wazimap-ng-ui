1d6578f2c48b7fd0e335d3a11c0ac52c6ed19c06
JD Bothma <jbothma@gmail.com>
2022-06-11 15:52:54 +0200

    Add template tags using webflow-import-export

--- src/js/webflow/import.js
+++ src/js/webflow/import.js
@@ -1,5 +1,9 @@
-exports.transform = function (window, $) {
+exports.transformDOM = function(window, $) {
+  $("title").text("{{ title }}");
+  $('meta[property="og:title"]').attr("content", "{{ title }}");
+  $('meta[property="twitter:title"]').attr("content", "{{ title }}");
+
   const tag = window.document.createElement("script");
   tag.setAttribute("src", "js/index.js");
   window.document.body.appendChild(tag);
-}
+};

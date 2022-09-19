9aa02f2ad5723d0b879055e8b2f8c9a53dc0ef1a
emre2038 <emreyildirim2038@gmail.com>
2022-09-19 21:41:49 +0300

    testing if the themes and categories are colored correctly

--- __tests__/gui/point_data/point_data.js
+++ __tests__/gui/point_data/point_data.js
@@ -37,8 +37,16 @@ When('I expand Higher Education theme', () => {
 })
 
 Then('I select TVET colleges category', () => {
+    cy.get('.point-mapper__h2:contains("TVET colleges")')
+        .closest('.point-mapper__h1')
+        .find('.point-mapper__h1_trigger')
+        .should('have.css', 'color')
+        .and('eq', 'rgb(34, 166, 179)');
     cy.get('.point-mapper__h2:contains("TVET colleges")').click();
     cy.get('.point-mapper__h2:contains("TVET colleges") .point-mapper__h2_load-complete').should('be.visible');
+    cy.get('.point-mapper__h2:contains("TVET colleges")')
+        .should('have.css', 'background-image')
+        .and('eq', 'linear-gradient(rgb(144, 210, 217), rgb(144, 210, 217))');
 })
 
 Then('I deselect TVET colleges category', () => {
@@ -68,8 +76,16 @@ When('I expand Labour theme', () => {
 })
 
 Then('I select Additional DEL facilities category', () => {
+    cy.get('.point-mapper__h2:contains("Additional DEL facilities (unverified)")')
+        .closest('.point-mapper__h1')
+        .find('.point-mapper__h1_trigger')
+        .should('have.css', 'color')
+        .and('eq', 'rgb(235, 77, 75)');
     cy.get('.point-mapper__h2:contains("Additional DEL facilities (unverified)")').click();
     cy.get('.point-mapper__h2:contains("Additional DEL facilities (unverified)") .point-mapper__h2_load-complete').should('be.visible');
+    cy.get('.point-mapper__h2:contains("Additional DEL facilities (unverified)")')
+        .should('have.css', 'background-image')
+        .and('eq', 'linear-gradient(rgb(245, 166, 165), rgb(245, 166, 165))');
 })
 
 Then('I deselect Additional DEL facilities category', () => {
@@ -79,14 +95,14 @@ Then('I deselect Additional DEL facilities category', () => {
 When('I check if the cluster is created correctly', () => {
     const categories = [{
         name: 'Additional DEL facilities (unverified)',
-        color: 'rgb(153, 58, 255)',
+        color: 'rgb(235, 77, 75)',
         circleVal: '100 100',
-        hex: "#993aff"
+        hex: "#eb4d4b"
     }, {
         name: 'TVET colleges',
-        color: 'rgb(58, 112, 255)',
+        color: 'rgb(34, 166, 179)',
         circleVal: '50 100',
-        hex: "#3a70ff"
+        hex: "#22a6b3"
     }]
 
     cy.get('.leaflet-clusters-pane .leaflet-zoom-animated svg text', {timeout: 20000}).should('have.text', 2);

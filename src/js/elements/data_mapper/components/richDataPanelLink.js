dd81257c474aab62d42bdac65f70cff2791cf15c
Gaurav <goyal1092@gmail.com>
2022-11-03 18:14:56 +0530

    Updated accoridng to review

--- src/js/elements/data_mapper/components/richDataPanelLink.js
+++ src/js/elements/data_mapper/components/richDataPanelLink.js
@@ -29,8 +29,7 @@ const RichDataPanelLink = (props) => {
         </IconContainer>
         <TextContainer>
           <p>
-            There are indicators for selected geography in the
-            <Link
+            There are indicators for selected geography in the <Link
               href="#"
               onClick={() => openRichDataPanel()}
             > <PanelIconLink />Rich Data View

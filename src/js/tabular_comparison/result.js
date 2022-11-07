b763a987f42125a1afaf735557877c8480f11ad3
emre2038 <emreyildirim2038@gmail.com>
2022-11-07 18:23:35 +0300

    coloring the table cells

--- src/js/tabular_comparison/result.js
+++ src/js/tabular_comparison/result.js
@@ -1,24 +1,67 @@
 import React, {useState, useEffect} from "react";
 import {Card, Grid, TableContainer, Table, TableBody, TableCell, Paper, TableHead, TableRow} from "@mui/material";
 import {format as d3format} from "d3-format";
+import {scaleSequential as d3scaleSequential} from 'd3-scale';
 
 //components
 import SectionTitle from "./section-title";
 
 const Result = (props) => {
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+        let rows = [];
+        props.selectedGeographies.map((geo) => {
+            let newRow = {geo: geo.name, objs: []};
+            props.indicatorObjs.map((obj) => {
+                let newObj = {
+                    obj: obj,
+                    value: calculateValue(geo, obj),
+                    formatting: ',.0f'  //todo : update this
+                }
+                newRow.objs.push(newObj);
+            })
+
+            rows.push(newRow);
+        })
+
+        let darkestColor = '#BABABA';
+        let lightestColor = '#ffffff';
+
+        props.indicatorObjs.forEach((obj) => {
+            let objValues = [];
+            rows.forEach((row) => {
+                let objValue = row.objs.filter(x => x.obj === obj)[0]?.value;
+                objValues.push(objValue);
+            })
+
+            const max = Math.max(...objValues);
+            const min = Math.min(...objValues);
+
+            const scale = d3scaleSequential()
+                .domain([min, max])
+                .range([lightestColor, darkestColor]);
+
+            rows.forEach((row) => {
+                let rowObj = row.objs.filter(x => x.obj === obj)[0];
+                if (rowObj != null) {
+                    rowObj.background = scale(rowObj.value);
+                    rowObj.value = d3format(rowObj.formatting)(rowObj.value);
+                }
+            })
+        })
+        console.log({rows});
+
+        setRows(rows);
+    }, [props.indicatorObjs, props.indicators, props.selectedGeographies])
+
     const calculateFullHeight = () => {
         const topSpace = 56 + 60;
         const bottomSpace = 40;
         return window.innerHeight - topSpace - bottomSpace;
     }
 
-    const calculateTableHeight = () => {
-        const headerHeight = 58;
-
-        return calculateFullHeight() - headerHeight;
-    }
-
-    const displayValue = (geo, obj) => {
+    const calculateValue = (geo, obj) => {
         let selectedIndicator = props.indicators.filter((ind) => ind.geo === geo.code && ind.indicator === obj.indicator)[0];
         if (selectedIndicator == null) {
             return;
@@ -26,16 +69,9 @@ const Result = (props) => {
 
         const primaryGroup = selectedIndicator.indicatorDetail.metadata?.primary_group;
         const data = selectedIndicator.indicatorDetail.data?.filter(x => x[primaryGroup] === obj.category);
-        const formatting = selectedIndicator.indicatorDetail.chart_configuration?.types.Value.formatting;
         let value = data?.reduce((n, {count}) => n + count, 0);
 
-        return (
-            <TableCell
-                component={'th'}
-                scope={'row'}
-                key={obj.index}
-            >{formatting === undefined ? value : d3format(formatting)(value)}</TableCell>
-        )
+        return value;
     }
 
     return (
@@ -66,33 +102,47 @@ const Result = (props) => {
                                 <TableRow>
                                     <TableCell
                                     ><b>Geography</b></TableCell>
-                                    {props.indicatorObjs.map((column) => {
-                                        return (
-                                            <TableCell
-                                                key={column.index}
-                                                className={'truncate-table-cell'}
-                                                title={column.indicator + ' : ' + column.category}
-                                            ><b>{column.indicator} : {column.category}</b></TableCell>
-                                        )
-                                    })}
+                                    {
+                                        props.indicatorObjs.map((column) => {
+                                            return (
+                                                <TableCell
+                                                    key={column.index}
+                                                    className={'truncate-table-cell'}
+                                                    title={column.indicator + ' : ' + column.category}
+                                                ><b>{column.indicator} : {column.category}</b></TableCell>
+                                            )
+                                        })
+                                    }
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {props.selectedGeographies.map((geo) => {
-                                    return (
-                                        <TableRow
-                                            key={geo.code}
-                                        >
-                                            <TableCell
-                                                component="th"
-                                                scope="row"
-                                            >{geo.name}</TableCell>
-                                            {
-                                                props.indicatorObjs.map((obj) => displayValue(geo, obj))
-                                            }
-                                        </TableRow>
-                                    )
-                                })}
+                                {
+                                    rows.map((row) => {
+                                        return (
+                                            <TableRow
+                                                key={row.geo}
+                                            >
+                                                <TableCell
+                                                    component={'th'}
+                                                    scope={'row'}
+                                                >{row.geo}</TableCell>
+                                                {
+                                                    props.indicatorObjs.map((obj) => {
+                                                        return (
+                                                            <TableCell
+                                                                component={'th'}
+                                                                scope={'row'}
+                                                                key={obj.index}
+                                                                sx={{backgroundColor: row.objs.filter(x => x.obj === obj)[0]?.background}}
+                                                            >{row.objs.filter(x => x.obj === obj)[0]?.value}
+                                                            </TableCell>
+                                                        )
+                                                    })
+                                                }
+                                            </TableRow>
+                                        )
+                                    })
+                                }
                             </TableBody>
                         </Table>
                     </TableContainer>

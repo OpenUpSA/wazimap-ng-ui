8641b35b8e8405552c3febd5abf68a782331890b
emre2038 <emreyildirim2038@gmail.com>
2022-11-09 11:00:47 +0300

    empty states

--- src/js/tabular_comparison/comparison-indicators.js
+++ src/js/tabular_comparison/comparison-indicators.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {Button, Card, Grid} from "@mui/material";
+import {Button, Card, Chip, Grid} from "@mui/material";
 import AddIcon from '@mui/icons-material/AddCircle';
 
 //components
@@ -8,6 +8,19 @@ import Indicator from "./indicator";
 
 const ComparisonIndicators = (props) => {
     const [previousSelectedGeographies, setPreviousSelectedGeographies] = useState([]);
+    const arrowSvg =
+        <svg width="68" height="147" viewBox="0 0 68 147" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M58.9715 0.118419C58.4845 -0.141979 57.8785 0.0417527 57.6181 0.528796L53.3747 8.46562C53.1143
+            8.95267 53.298 9.55859 53.7851 9.81899C54.2721 10.0794 54.878 9.89565 55.1384 9.40861L58.9104 2.35365L65.9654
+            6.1256C66.4524 6.38599 67.0583 6.20226 67.3187 5.71522C67.5791 5.22818 67.3954 4.62225 66.9083 4.36186L58.9715
+            0.118419ZM5.90373 145.572C3.16222 139.784 2.29581 134.458 2.79782 129.41C3.30085 124.351 5.18405 119.511
+            8.03179 114.714C13.7576 105.068 23.2348 95.8083 32.8583 85.43C42.4299 75.1077 52.0678 63.7485 57.8127
+            50.0589C63.5739 36.3306 65.4002 20.31 59.457 0.710109L57.543 1.29047C63.3498 20.4403 61.5512 35.9821
+            55.9685 49.285C50.3697 62.6266 40.9451 73.7675 31.3917 84.0701C21.8902 94.3169 12.1799 103.808 6.31197
+            113.693C3.36283 118.661 1.34759 123.782 0.807639 129.212C0.266685 134.652 1.21276 140.341 4.09625 146.428L5.90373 145.572Z"
+                  fill="#39AD84"/>
+        </svg>
+
 
     useEffect(() => {
         const addedGeographies = props.selectedGeographies.filter(({code: id1}) => !previousSelectedGeographies.some(({code: id2}) => id2 === id1));
@@ -83,6 +96,48 @@ const ComparisonIndicators = (props) => {
         props.setIndicatorObjs(newArr);
     }
 
+    const renderIndicators = () => {
+        if (props.selectedGeographies.length <= 0) {
+            return renderEmptyGeographyState();
+        } else if (props.indicatorObjs.length <= 0) {
+            return renderEmptyIndicatorState();
+        } else {
+            return renderAddedIndicators();
+        }
+    }
+
+    const renderAddedIndicators = () => {
+        return props.indicatorObjs.map((x) =>
+            <Indicator
+                key={x.index}
+                indicators={props.indicators}
+                handleRemove={() => handleRemove(x.index)}
+                handleIndicatorSelection={(newValue) => handleIndicatorSelection(x.index, newValue)}
+                handleCategorySelection={(newValue) => handleCategorySelection(x.index, newValue)}
+            />
+        )
+    }
+
+    const renderEmptyGeographyState = () => {
+        return (
+            <div
+                className={'indicators-empty-state'}
+                style={{marginTop: props.cardHeight / 2 - 8}}
+            >
+                <span>Indicators can only be selected once a geography is chosen!</span>
+            </div>
+        )
+    }
+
+    const renderEmptyIndicatorState = () => {
+        return (
+            <div className={'indicators-empty-state margin-top-10'}>
+                <div className={'margin-bottom-10'}>{arrowSvg}</div>
+                <div>Great! Now select an indicator for your chosen geography!</div>
+            </div>
+        )
+    }
+
     return (
         <Grid container>
             <Grid
@@ -99,6 +154,7 @@ const ComparisonIndicators = (props) => {
                     className={'full-width btn-add-indicator'}
                     startIcon={<AddIcon/>}
                     onClick={addIndicator}
+                    disabled={props.selectedGeographies.length <= 0}
                 >
                     Click to add an indicator to compare
                 </Button>
@@ -109,15 +165,7 @@ const ComparisonIndicators = (props) => {
                     variant={'outlined'}
                     sx={{height: props.cardHeight}}
                 >
-                    {props.indicatorObjs.map((x) =>
-                        <Indicator
-                            key={x.index}
-                            indicators={props.indicators}
-                            handleRemove={() => handleRemove(x.index)}
-                            handleIndicatorSelection={(newValue) => handleIndicatorSelection(x.index, newValue)}
-                            handleCategorySelection={(newValue) => handleCategorySelection(x.index, newValue)}
-                        />
-                    )}
+                    {renderIndicators()}
                 </Card>
             </Grid>
         </Grid>
--- src/js/tabular_comparison/result.js
+++ src/js/tabular_comparison/result.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react";
-import {Card, Grid, TableContainer, Table, TableBody, TableCell, Paper, TableHead, TableRow} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {Card, Chip, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import {format as d3format} from "d3-format";
 import {scaleSequential as d3scaleSequential} from 'd3-scale';
 
@@ -8,6 +8,31 @@ import SectionTitle from "./section-title";
 
 const Result = (props) => {
     const [rows, setRows] = useState([]);
+    const arrowSvg =
+        <svg width="213" height="290" viewBox="0 0 213 290" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M0.36742 6.7255C-0.0602784 7.07491 -0.123745 7.70489 0.225674 8.13259L5.91973 15.1024C6.26915
+                15.5301 6.89912 15.5935 7.32683 15.2441C7.75452 14.8947 7.81798 14.2647 7.46857 13.837L2.40718
+                7.64166L8.60253 2.58028C9.03024 2.23086 9.0937 1.60088 8.74428 1.17319C8.39487 0.745486 7.76489
+                0.682026 7.33719 1.03144L0.36742 6.7255ZM212.623 287.508C189.548 284.647 172.188 278.459 159.049
+                269.827C145.918 261.201 136.954 250.102 130.711 237.334C118.192 211.731 116.628 179.477 114.498
+                146.997C112.376 114.634 109.69 82.0613 94.6764 56.3165C79.6005 30.4642 52.2065 11.6532 1.10032
+                6.50495L0.899863 8.49488C51.5436 13.5966 78.2745 32.1606 92.9487 57.324C107.685 82.5948 110.374
+                114.679 112.502 147.128C114.622 179.461 116.183 212.176 128.914 238.213C135.297 251.266 144.488
+                262.655 157.951 271.499C171.406 280.338 189.077 286.604 212.377 289.493L212.623 287.508Z"
+                fill="#39AD84"/>
+        </svg>
+
+    const arrowSvg2 =
+        <svg width="263" height="57" viewBox="0 0 263 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.0557372 12.671C-0.126071 13.1925 0.149305 13.7626 0.670807 13.9444L9.16917 16.9072C9.69067
+            17.089 10.2608 16.8136 10.4426 16.2921C10.6244 15.7706 10.3491 15.2005 9.82756 15.0187L2.27346 12.3851L4.907
+            4.83101C5.0888 4.30951 4.81343 3.73936 4.29193 3.55756C3.77042 3.37575 3.20028 3.65112 3.01847
+             4.17263L0.0557372 12.671ZM262.293 51.0218C205.916 62.9429 164.549 41.9099 125.18 23.4163C85.8461
+             4.93904 48.4461 -11.0265 0.56508 12.0997L1.43492 13.9007C48.4 -8.78318 85.0001 6.75127 124.33
+             25.2265C163.625 43.6855 205.584 65.0574 262.707 52.9785L262.293 51.0218Z" fill="#39AD84"/>
+        </svg>
+
 
     useEffect(() => {
         populateRows();
@@ -83,9 +108,101 @@ const Result = (props) => {
 
         const primaryGroup = selectedIndicator.indicatorDetail.metadata?.primary_group;
         const data = selectedIndicator.indicatorDetail.data?.filter(x => x[primaryGroup] === obj.category);
-        let value = data?.reduce((n, {count}) => n + parseFloat(count), 0);
+        return data?.reduce((n, {count}) => n + parseFloat(count), 0);
+    }
 
-        return value;
+    const renderResult = () => {
+        if (props.selectedGeographies.length <= 0) {
+            return renderEmptyGeographyState();
+        } else if (props.indicatorObjs.length <= 0) {
+            return renderEmptyIndicatorState();
+        } else {
+            return renderTable();
+        }
+    }
+
+    const renderTable = () => {
+        return <TableContainer
+            component={Paper}
+            sx={{maxHeight: calculateFullHeight()}}
+        >
+            <Table
+                stickyHeader
+                sx={{minWidth: 650}}
+                aria-label={'simple table'}
+            >
+                <TableHead>
+                    <TableRow>
+                        <TableCell
+                        ><b>Geography</b></TableCell>
+                        {
+                            props.indicatorObjs.map((column) => {
+                                if (column.indicator !== '' && column.category !== '') {
+                                    return (
+                                        <TableCell
+                                            key={column.index}
+                                            className={'truncate-table-cell'}
+                                            title={column.indicator + ' : ' + column.category}
+                                        ><b>{column.indicator} : {column.category}</b></TableCell>
+                                    )
+                                }
+                            })
+                        }
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        rows.map((row) => {
+                            return (
+                                <TableRow
+                                    key={row.geo}
+                                >
+                                    <TableCell
+                                        component={'th'}
+                                        scope={'row'}
+                                    >{row.geo}</TableCell>
+                                    {
+                                        props.indicatorObjs.map((obj) => {
+                                            if (obj.indicator !== '' && obj.category !== '') {
+                                                return (
+                                                    <TableCell
+                                                        component={'th'}
+                                                        scope={'row'}
+                                                        key={obj.index}
+                                                        sx={{backgroundColor: row.objs.filter(x => x.obj === obj)[0]?.background}}
+                                                    >{row.objs.filter(x => x.obj === obj)[0]?.value}
+                                                    </TableCell>
+                                                )
+                                            }
+                                        })
+                                    }
+                                </TableRow>
+                            )
+                        })
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>
+    }
+
+    const renderEmptyGeographyState = () => {
+        return (
+            <div className={'result-empty-state'}>
+                <span>{arrowSvg}</span>
+                <span>We can’t wait to show you some data! Choose a geography to get started!</span>
+            </div>
+        )
+    }
+
+    const renderEmptyIndicatorState = () => {
+        return (
+            <div
+                className={'result-empty-state result-empty-indicators'}
+                 style={{marginTop: calculateFullHeight() / 2 - 8}}>
+                <span>{arrowSvg2}</span>
+                <span>You’ve chosen a geography, now select an indicator!</span>
+            </div>
+        )
     }
 
     return (
@@ -103,67 +220,7 @@ const Result = (props) => {
                     variant={'outlined'}
                     sx={{height: calculateFullHeight()}}
                 >
-                    <TableContainer
-                        component={Paper}
-                        sx={{maxHeight: calculateFullHeight()}}
-                    >
-                        <Table
-                            stickyHeader
-                            sx={{minWidth: 650}}
-                            aria-label={'simple table'}
-                        >
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell
-                                    ><b>Geography</b></TableCell>
-                                    {
-                                        props.indicatorObjs.map((column) => {
-                                            if (column.indicator !== '' && column.category !== '') {
-                                                return (
-                                                    <TableCell
-                                                        key={column.index}
-                                                        className={'truncate-table-cell'}
-                                                        title={column.indicator + ' : ' + column.category}
-                                                    ><b>{column.indicator} : {column.category}</b></TableCell>
-                                                )
-                                            }
-                                        })
-                                    }
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {
-                                    rows.map((row) => {
-                                        return (
-                                            <TableRow
-                                                key={row.geo}
-                                            >
-                                                <TableCell
-                                                    component={'th'}
-                                                    scope={'row'}
-                                                >{row.geo}</TableCell>
-                                                {
-                                                    props.indicatorObjs.map((obj) => {
-                                                        if (obj.indicator !== '' && obj.category !== '') {
-                                                            return (
-                                                                <TableCell
-                                                                    component={'th'}
-                                                                    scope={'row'}
-                                                                    key={obj.index}
-                                                                    sx={{backgroundColor: row.objs.filter(x => x.obj === obj)[0]?.background}}
-                                                                >{row.objs.filter(x => x.obj === obj)[0]?.value}
-                                                                </TableCell>
-                                                            )
-                                                        }
-                                                    })
-                                                }
-                                            </TableRow>
-                                        )
-                                    })
-                                }
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
+                    {renderResult()}
                 </Card>
             </Grid>
         </Grid>

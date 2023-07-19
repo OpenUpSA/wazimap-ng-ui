c8d244683fe9bd8bad53387837460f61cac2a5fd
Gaurav Goyal <goyal1092@gmail.com>
2023-07-19 16:50:40 +0530

    Updated css of tutorial button

--- src/js/elements/tutorial/tutorial_button.js
+++ src/js/elements/tutorial/tutorial_button.js
@@ -6,18 +6,22 @@ import {styled} from "@mui/system";
 const StyledButton = styled(Button)(() => ({
     backgroundColor: '#39ad84',
     fontWeight: '700',
+    fontSize: '14px',
     color: '#fff',
     textTransform: 'none',
     right: 0,
     transform: 'rotate(-90deg) translate(0, -100%)',
     transformOrigin: '100% 0',
     position: 'fixed',
-    height: '44px',
+    padding: '6px 7px',
+    border: 'none',
+    borderRadius: '2px',
+    height: '43px',
     [`& svg`]: {
         marginRight: '5px'
     },
     '&:hover': {
-        backgroundColor: '#39ad84'
+        backgroundColor: '#39ad84',
     }
 }));
 
@@ -29,7 +33,7 @@ const TutorialButton = (props) => {
                 bottom: props.feedbackBtnVisible ? '300px' : '200px'
             }}
         >
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="20">
                 <path d="M0 0h24v24H0z" fill="none"></path>
                 <path fill="currentColor"
                       d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"></path>

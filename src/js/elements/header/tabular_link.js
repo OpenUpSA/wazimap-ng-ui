1e9e51397656f61558c2537256db8ebaccdfc3b8
Emre YILDIRIM <emreyildirim2038@gmail.com>
2023-08-25 13:00:22 +0300

    open links in new tab

--- src/js/elements/header/tabular_link.js
+++ src/js/elements/header/tabular_link.js
@@ -29,7 +29,7 @@ const StyledButton = styled(Button)(() => ({
 
 const TabularButton = () => {
     return (
-        <StyledButton href="tabular-comparison.html" title={'Compare multiple indicators for multiple geographic areas side-by-side'}>
+        <StyledButton target={'_blank'} href="tabular-comparison.html" title={'Compare multiple indicators for multiple geographic areas side-by-side'}>
             Compare <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
             <path d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5
             32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80
